e7e9d1595ba164e5d3ef481c6eecd9fc4da0fbaf
Vinicius <vctelles09@gmail.com>
2021-12-09 02:56:52 -0300

    Primeiro fecth dos paises

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://testefront.dev.softplan.com.br/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,48 @@
+import { useQuery, gql } from "@apollo/client";
 import { CountryCard } from "../../components/CountryCard";
 import { SearchComponent } from "../../components/SearchComponent";
+import { useEffect } from 'react';
+
+interface CountryProps {
+    _id: number;
+    name: string;
+    capital: string;
+    flag: {
+        svgFile: string;
+    }
+}
 
 
 export function Home() {
+
+    
+    const { loading, error, data } = useQuery(gql`
+    query CountryList {
+        Country {
+            _id,
+            name,
+            capital,
+            flag {
+                svgFile
+            },
+        }
+    }
+`);
+    console.log(data)
     return(
         <main className="w-screen h-screen bg-gray-700 flex p-8">
             <div className="w-full flex flex-col">
                 <SearchComponent />
-                <CountryCard />
+                {data?.Country.map(({ _id, name, capital, flag }: CountryProps) => (
+                    <CountryCard
+                        key={_id}
+                        _id={_id}
+                        name={name}
+                        capital={capital}
+                        flag={flag.svgFile}
+                    />
+                ))}
+                
             </div>
         </main>
     )
